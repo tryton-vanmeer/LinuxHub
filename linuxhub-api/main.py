1fbdd7c32eb844d0f2fea9d0c7b3e9d42962865c
Tryton Van Meer <trytonvanmeer@protonmail.com>
2020-04-23 15:37:44 -0400

    Refactor 'get_os_list_by_distro' to 'get_distro_os_list'.

--- linuxhub-api/main.py
+++ linuxhub-api/main.py
@@ -17,7 +17,7 @@ def read_distros():
 @app.get("/distro/{distro_id}")
 def read_distro(distro_id: Distros):
     data = []
-    os_list = osinfo.get_os_list_by_distro(distro_id)
+    os_list = osinfo.get_distro_os_list(distro_id)
 
     for os in os_list:
         data.append({"id": os.get_short_id(), "name": os.get_name()})
