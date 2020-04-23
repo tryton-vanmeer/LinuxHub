3c5ab87d0c4fe43b5bc530f16e2b1401efdd9ed9
Tryton Van Meer <trytonvanmeer@protonmail.com>
2020-04-23 14:36:08 -0400

    Itterate through Distro enum for /distro endpoint.

--- linuxhub-api/main.py
+++ linuxhub-api/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
-from osinfo import OSInfo
+from osinfo import OSInfo, Distros
 
 app = FastAPI()
 osinfo = OSInfo()
 
 
 @app.get("/distro")
-def read_distro():
-    return osinfo.DISTROS
+def read_distros():
+    data = []
+    for distro in Distros:
+        data.append({"id": distro.name, "name": distro.value})
+
+    return data
+
+
+@app.get("/distro/{id}")
+def read_distro(id: str):
+    pass
