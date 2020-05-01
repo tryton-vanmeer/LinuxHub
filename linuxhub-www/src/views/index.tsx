4d62315a34b930b2fb0f5bd4807327ca5418a517
Tryton Van Meer <trytonvanmeer@protonmail.com>
2020-05-01 19:39:41 -0400

    Add main with imagelinks for Distros in index view.

--- linuxhub-www/src/views/index.tsx
+++ linuxhub-www/src/views/index.tsx
@@ -7,11 +7,39 @@ class Index extends React.Component<{}, {}> {
         return (
             <>
             <Header />
-            <ImageLink
-                link="/fedora"
-                title="Fedora"
-                img="https://gitlab.gnome.org/GNOME/gnome-boxes-logos/-/raw/master/logos/fedora.svg"
-            />
+
+            <main id="view-index">
+                <div className="columns is-desktop">
+                    <div className="column">
+                        <ImageLink
+                            link="/fedora"
+                            title="Fedora"
+                            img="https://gitlab.gnome.org/GNOME/gnome-boxes-logos/-/raw/master/logos/fedora.svg"
+                        />
+                    </div>
+                    <div className="column">
+                        <ImageLink
+                            link="/silverblue"
+                            title="Silverblue"
+                            img="https://gitlab.gnome.org/GNOME/gnome-boxes-logos/-/raw/master/logos/silverblue.svg"
+                        />
+                    </div>
+                    <div className="column">
+                        <ImageLink
+                            link="/ubuntu"
+                            title="Ubuntu"
+                            img="https://gitlab.gnome.org/GNOME/gnome-boxes-logos/-/raw/master/logos/ubuntu.svg"
+                        />
+                    </div>
+                    <div className="column">
+                        <ImageLink
+                            link="/debian"
+                            title="Debian"
+                            img="https://gitlab.gnome.org/GNOME/gnome-boxes-logos/-/raw/master/logos/debian.svg"
+                        />
+                    </div>
+                </div>
+            </main>
             </>
         )
     }
