71391978e2fff3f11c4a5d3055f6f9b8955ebc7a
Tryton Van Meer <trytonvanmeer@protonmail.com>
2020-05-01 19:03:53 -0400

    Render views/index.

--- linuxhub-www/src/index.tsx
+++ linuxhub-www/src/index.tsx
@@ -1,9 +1,10 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import App from "./components/app";
+import Index from "./views/index"
 import "./sass/main.scss";
 
 ReactDOM.render(
-    <App />,
+    <Index />,
     document.getElementById('root')
 );
