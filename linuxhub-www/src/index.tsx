b5bc6e9aa15f3bae9bf53dd97ce0605ec5b24b60
Tryton Van Meer <trytonvanmeer@protonmail.com>
2020-05-01 21:25:00 -0400

    Use a Router.

--- linuxhub-www/src/index.tsx
+++ linuxhub-www/src/index.tsx
@@ -1,9 +1,17 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import {BrowserRouter, Router, Route, Switch} from "react-router-dom";
+import { createBrowserHistory } from 'history'
 import Index from "./views/index"
 import "./sass/main.scss";
 
-ReactDOM.render(
-    <Index />,
-    document.getElementById('root')
-);
+ReactDOM.render((
+    <BrowserRouter>
+        <Router history={createBrowserHistory()}>
+            <Switch>
+                <Route exact path="/" component={Index} />
+                <Route exact path="/distro" component={Index} />
+            </Switch>
+        </Router>
+    </BrowserRouter>
+), document.getElementById("root"));
