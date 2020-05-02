a8acf46628357841c5c4d8745ea8d73f20e54263
Tryton Van Meer <trytonvanmeer@protonmail.com>
2020-05-01 21:29:21 -0400

    Moved views to routes and renamed index to home.

--- linuxhub-www/src/index.tsx
+++ linuxhub-www/src/index.tsx
@@ -3,7 +3,7 @@ import * as ReactDOM from "react-dom";
 import {BrowserRouter, Router, Route, Switch} from "react-router-dom";
 import { createBrowserHistory } from 'history'
 import Header from "./components/header";
-import Index from "./views/index"
+import Home from "./routes/home"
 import "./sass/main.scss";
 
 ReactDOM.render((
@@ -12,8 +12,8 @@ ReactDOM.render((
 
         <Router history={createBrowserHistory()}>
             <Switch>
-                <Route exact path="/" component={Index} />
-                <Route exact path="/distro" component={Index} />
+                <Route exact path="/" component={Home} />
+                <Route exact path="/distro" component={Home} />
             </Switch>
         </Router>
     </BrowserRouter>
