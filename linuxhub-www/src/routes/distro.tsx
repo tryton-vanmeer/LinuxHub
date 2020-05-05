cfc505e2372833e02cae19013aa88c39eedb1e64
Tryton Van Meer <trytonvanmeer@protonmail.com>
2020-05-05 14:03:02 -0400

    Setup testing JSON call to linuxhub-api.

--- linuxhub-www/src/routes/distro.tsx
+++ linuxhub-www/src/routes/distro.tsx
@@ -3,12 +3,34 @@ import { RouteProps } from 'react-router';
 import { withRouter } from "react-router-dom";
 
 class Distro extends React.Component<RouteProps, {}> {
-    public render() {
-        const { id } = this.props.match.params;
+    state = {
+        id: "",
+        data: []
+    }
 
+    public componentDidMount() {
+        const id = this.props.match.params.id;
+
+        fetch("/api/distro/" + id)
+        .then(res => res.json())
+        .then((data) => {
+            this.setState({
+                id: id,
+                data: data
+            });
+            console.log(data);
+        })
+        .catch(console.log);
+    }
+
+    public render() {
         return (
             <main>
-                <h1 class="distro-header">{id}</h1>
+                <h1 className="distro-header">{this.state.id}</h1>
+
+                <div>
+                    {JSON.stringify(this.state.data, null, 2)}
+                </div>
             </main>
         );
     }
