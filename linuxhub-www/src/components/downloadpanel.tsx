6634dd193ac4869269344a977e43a954bc1a2d48
Tryton Van Meer <trytonvanmeer@protonmail.com>
2020-05-07 12:38:04 -0400

    Added get_name function for returning the display name if media.name == null.

--- linuxhub-www/src/components/downloadpanel.tsx
+++ linuxhub-www/src/components/downloadpanel.tsx
@@ -12,6 +12,30 @@ interface DownloadPanelProps {
 }
 
 class DownloadPanel extends React.Component<DownloadPanelProps, {}> {
+    private get_name(media: Media) {
+        var name = media.name;
+
+        if (name == null) {
+            // Get the filename
+            var name = Path.basename(media.url);
+
+            // Remove the extension
+            name = name.slice(0, -4);
+
+            //Replace hyphens with spaces, except between numbers
+            name = name.replace(new RegExp("([a-z])[-]([a-z])", "gi"), "$1 $2");
+            name = name.replace(new RegExp("([a-z])[-]([0-9])", "gi"), "$1 $2");
+            name = name.replace(new RegExp("([0-9])[-]([a-z])", "gi"), "$1 $2");
+
+            // Remove arch
+            name = name.replace(new RegExp("x86_64[-]?"), "");
+            name = name.replace(new RegExp("aarch64[-]?"), "");
+            name = name.replace(new RegExp("armhfp[-]?"), "");
+        }
+
+        return name;
+    }
+
     public render() {
         const { media } = this.props;
 
