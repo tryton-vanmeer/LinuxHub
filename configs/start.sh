6610f1902b4df772ab78810ddb85d370ce73b6ed
Tryton Van Meer <trytonvanmeer@protonmail.com>
2020-05-08 17:09:27 -0400

    Bind gunicorn to a socket and start nginx.

--- configs/start.sh
+++ configs/start.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/sh
 
 cd /app/api || exit
-gunicorn -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8080 main:app
+# gunicorn -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8080 main:app
+gunicorn -D -k uvicorn.workers.UvicornWorker -b unix:/run/api.sock main:app
+
+nginx -g "daemon off;"
