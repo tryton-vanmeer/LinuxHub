9a9b5238b45d7bb3bc38c6de11e45486bf30ca07
Tryton Van Meer <trytonvanmeer@protonmail.com>
2020-05-19 12:07:51 -0400

    Remove unused configs.

--- configs/start.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/sh
-
-cd /app/api || exit
-# gunicorn -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8080 main:app
-gunicorn -D -k uvicorn.workers.UvicornWorker -b unix:/run/api.sock main:app
-
-nginx -g "daemon off;"
